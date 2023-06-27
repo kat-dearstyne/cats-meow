a9d83ad0ba24b176eeb4609bb3b1be4de0372149
Annalisa Szymanski <aszyman2@nd.edu>
2023-06-27 16:00:54 -0400

    updated AppRouter.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import React from "react";
 import AppRouter from "./AppRouter";
 import Navbar from "./components/Navbar/Navbar";
@@ -5,18 +6,24 @@ import Footer from "./components/Footer/Footer";
 import * as Env from "./environment";
 import Parse from "parse";
 
+// Initialize Parse with application id and javascript key from environment variables
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+// Set Parse server URL from environment variables
 Parse.serverURL = Env.SERVER_URL;
 
 function App() {
     return (
+        // This creates a flex container with column direction.  The height is set at 
         <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+            {/* Header section for Navbar. The flex property is used to specify the size of the component */}
             <header style={{ flex: "0 0 auto" }}>
                 <Navbar />
             </header>
+            {/* Main content section where different routes are rendered. It grows to take up all available space in the container */}
             <section id={"main"} style={{ flex: "1 0 auto" }}>
                 <AppRouter />
             </section>
+            {/* Footer section for Footer. The flex property is used to specify the size of the component */}
             <footer style={{ flex: "0 0 auto" }}>
                 <Footer />
             </footer>
@@ -24,4 +31,5 @@ function App() {
     );
 }
 
+// Export the App component to be used in other parts of the application
 export default App;
--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,4 @@
+// Used to initialize and set the server URL for Parse
 module.exports = {
     APPLICATION_ID: "6tYzd6o09XB44BFCw2MJ3QNAYKQY1TAX7i2EszBr",
     JAVASCRIPT_KEY: "CoUWeGRBfJwxtn6FAlHrotsvfQUzthauxRS3gmCy",
