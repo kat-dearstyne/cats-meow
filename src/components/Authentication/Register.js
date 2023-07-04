aace11f18ab0bc2a0d51ee7eecd56dce5f217381
kat-dearstyne <katherine@jklfamily.com>
2023-07-04 12:07:57 -0400

    update styling of register/login to match rest of site

--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import authService from '../../services/AuthServices';
 import AuthForm from './AuthForm';
+import {useNavigate} from "react-router-dom";
 
 function Register() {
   const [user, setUser] = useState({ firstName: '', lastName: '', email: '', password: '' });
+
+  const navigate = useNavigate(); // Use the useNavigate hook
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -15,14 +18,21 @@ function Register() {
     }
   };
 
+  const handleLoginRedirect = () => {
+    navigate("/login"); // Redirect to the register page
+  };
+
   const handleChange = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
 
   return (
     <div>
+      <h2>Welcome to Cats Meow!</h2>
       <h2>Please fill out the fields below to register.</h2>
       <AuthForm user={user} onChange={handleChange} onSubmit={handleRegister} buttonText="Register" />
+      <p>Already have an account? <button onClick={handleLoginRedirect}>Login in</button></p>
+
     </div>
   );
 }
